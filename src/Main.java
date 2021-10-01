1b30d4798be43eb378b3074a82adeb438bcf8c35
Ilya <ilakosov8@gmail.com>
2021-10-01 14:29:51 +0700

    five commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int priceOfTicketInRub = 15000;
+        int amountOfRubInOneMile = 20;
+        int miles =priceOfTicketInRub / amountOfRubInOneMile;
+        System.out.println(miles);
+    }
+
 }
